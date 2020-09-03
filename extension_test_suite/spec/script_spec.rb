78f7f697ddd94f41865fb67276ac0f16c957d49b
Michael Wells <mwells1286@gmail.com>
2020-09-02 20:55:25 -0700

    update test scripts

--- extension_test_suite/spec/script_spec.rb
+++ extension_test_suite/spec/script_spec.rb
@@ -3,6 +3,11 @@ require 'feature_helper'
 @sites = File.read('sites.txt').split
 @sites.each do |url|
   RSpec.feature "#{url} - Extension Script" do
+    before(:each) do
+      visit 'http://tilig-api:3000'
+      sleep 1
+    click_on 'Sign in with Google'
+    end
     scenario 'places logo in login' do
       visit url
       expect(page).to have_selector('div.subhq-subs-icon')
@@ -10,7 +15,11 @@ require 'feature_helper'
     scenario 'places dropdown on login when selected' do
       visit url
       first('input[data-comsubshqforms]').click
+      sleep 1
       expect(page).to have_selector('div.subshq-dropdown-suggestions')
+      sleep 1
+      expect(page).to have_content( 'Add to Tilig')
     end
+    
   end
 end
