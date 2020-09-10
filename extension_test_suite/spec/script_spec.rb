7f66df186602b75ea1dfe9d9ac85fc62e2deb949
Michael Wells <mwells1286@gmail.com>
2020-09-09 21:23:56 -0700

    remove breaking tests

--- extension_test_suite/spec/script_spec.rb
+++ extension_test_suite/spec/script_spec.rb
@@ -1,5 +1,4 @@
 require 'feature_helper'
-p 'FULL SCAN'
 @sites = File.read('sites.txt').split
 @sites.each do |url|
   RSpec.feature "#{url} - Extension script" do
@@ -7,16 +6,19 @@ p 'FULL SCAN'
       visit 'http://tilig-api:3000'
       click_on 'Sign in with Google'
       visit url
-      p url
     end
-  
+
+    scenario 'places logo in login' do
+      visit url
+      expect(page).to have_selector('div.subhq-subs-icon')
+    end
     scenario 'places dropdown on login when selected' do
       first('input[data-comsubshqforms]').click
       expect(page).to have_selector('div.subshq-dropdown-suggestions')
       expect(page).to have_content('Add to Tilig')
     end
 
-    scenario 'open saves login modal if user does not have suggested password' do
+    scenario 'open save login modal if user does not have suggested password' do
       first('input[data-comsubshqforms]').click
       find('a.subshq-button').click
       expect(page).to have_content('Save Login')
