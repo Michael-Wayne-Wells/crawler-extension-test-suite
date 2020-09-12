84b9354baf1c24bb98c0fafe0908c408f1e84c29
Michael Wells <mwells1286@gmail.com>
2020-09-12 11:47:25 -0700

    add tests to feature folder

---
+++ extension_test_suite/spec/features/script_spec.rb
@@ -0,0 +1,26 @@
+require 'feature_helper'
+@sites = File.read('sites.txt').split
+@sites.each do |url|
+  RSpec.feature "#{url} - Extension script" do
+    before(:each) do
+      visit 'http://tilig-api:3000'
+      click_on 'Sign in with Google'
+      visit url
+    end
+
+    scenario 'places dropdown on login when selected' do
+      first('input[data-comsubshqforms]').click
+      expect(page).to have_selector('div.subshq-dropdown-suggestions')
+      expect(page).to have_content('Add to Tilig')
+    end
+
+    scenario 'open save login modal if user does not have suggested password' do
+      first('input[data-comsubshqforms]').click
+      find('a.subshq-button').click
+      expect(page).to have_content('Save Login')
+      expect(page).to have_content('Save')
+      expect(page).to have_content('Cancel')
+      expect(page).to have_selector('img.modal-logo')
+    end
+  end
+end
